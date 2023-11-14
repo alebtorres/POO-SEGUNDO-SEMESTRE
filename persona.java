84a3f1652a1f9f561a082a2d2d66f07847a8f0c1
alebtorres <alexbordonez@gmail.com>
2023-11-14 17:26:47 -0500

    Creción de clase persona con sus atributos y metodos

---
+++ persona.java
@@ -0,0 +1,28 @@
+public class persona {
+    String nombre;
+    int edad;
+
+    public persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public void saltar() {
+        System.out.println(this.nombre + " empezo a saltar");
+    }
+
+    public void comer() {
+        System.out.println(this.nombre + " empezo a comer");
+    }
+
+    public void nadar() {
+        System.out.println(this.nombre + " empezo a nadar");
+    }
+
+    public static void main(String[] args) {
+        persona p2 = new persona("Alexander ", 19);
+        p2.saltar();
+        p2.comer();
+        p2.nadar();
+    }
+}
