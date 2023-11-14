69aa59e2df5f693f829d9cc938aa5f6a7e3e3eec
alebtorres <alexbordonez@gmail.com>
2023-11-14 15:55:11 -0500

    Codigo de clase gat1 con sus atributos y metodos

---
+++ gato1.java
@@ -0,0 +1,38 @@
+public class gato1 {
+    String nombre;
+    int edad;
+    String color;
+    String altura;
+
+    public gato1(String nombre, int edad, String color, String altura) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.color = color;
+        this.altura = altura;
+    }
+
+    public void maullar() {
+        System.out.println(this.nombre + "esta maullando");
+    }
+
+    public void saltar() {
+        System.out.println(this.nombre + "esta saltando");
+    }
+
+    public void correr() {
+        System.out.println(this.nombre + "esta corriendo");
+    }
+
+    public void dormir() {
+        System.out.println(this.nombre + "esta durmiendo");
+    }
+
+    public static void main(String[] args) {
+        gato1 gat1 = new gato1("Alfred ", 2, "Gris con rayas negras", "32cm");
+        gat1.maullar();
+        gat1.saltar();
+        gat1.correr();
+        gat1.dormir();
+    }
+
+}
