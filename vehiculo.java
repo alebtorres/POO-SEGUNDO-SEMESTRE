391e282ae16b0d1449f90c42661a6bbdad08c4de
alebtorres <alexbordonez@gmail.com>
2023-11-14 17:31:13 -0500

    Creación de clase vehiculo aplicando función if para imponer multa si excede velocidad

---
+++ vehiculo.java
@@ -0,0 +1,24 @@
+public class vehiculo {
+    String marca;
+    int velocidad;
+    String placa;
+
+    public vehiculo(String marca, int velocidad, String placa) {
+        this.marca = marca;
+        this.velocidad = velocidad;
+        this.placa = placa;
+    }
+
+    public void verificarVelocidad() {
+        if (velocidad > 50) {
+            System.out.println("Multa aplicada al " + marca + " de placa: " + placa + " ,por exceso de velocidad");
+        } else {
+            System.out.println("El vehiculo " + marca + " recorre a una velocidad permitida");
+        }
+    }
+
+    public static void main(String[] args) {
+        vehiculo porche1 = new vehiculo("Porsche", 100, "ABB-1892");
+        porche1.verificarVelocidad();
+    }
+}
