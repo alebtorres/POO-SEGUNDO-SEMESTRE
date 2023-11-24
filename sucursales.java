bb59f920fcdc020f5428d6a83545984ec760ff53
alebtorres <alexanderbordonez@gmail.com>
2023-11-23 20:04:16 -0500

    Cambio en el codigo, depuración

--- sucursales.java
+++ sucursales.java
@@ -13,34 +13,26 @@ public class sucursales {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Por favor, elige una sucursal: 1, 2 o 3");
-        String sucursal_s = scanner.nextLine();
+        sucursal = scanner.nextInt();
 
-        // Validar si la entrada del usuario es un número entero
-        if (sucursal_s.matches("[0-9]+")) {
-            sucursal = Integer.parseInt(sucursal_s);
-
-            switch (sucursal) {
-                case 1:
-                    System.out.println("Has elegido la sucursal 1");
-                    break;
-                case 2:
-                    System.out.println("Has elegido la sucursal 2");
-                    break;
-                case 3:
-                    System.out.println("Has elegido la sucursal 3");
-                    break;
-                default:
-                    System.out.println("Opción no valida");
-                    break;
-            }
-        } else {
-            System.out.println("Opción no valida");
+        switch (sucursal) {
+            case 1:
+                System.out.println("Has elegido la sucursal 1");
+                break;
+            case 2:
+                System.out.println("Has elegido la sucursal 2");
+                break;
+            case 3:
+                System.out.println("Has elegido la sucursal 3");
+                break;
+            default:
+                System.out.println("Opción no valida");
+                break;
         }
 
         System.out.println("Por favor, elige un producto: A, B o C");
-        producto = scanner.nextLine();
+        producto = scanner.next();
 
-        // Convertir la entrada del usuario a mayúsculas
         producto = producto.toUpperCase();
 
         switch (producto) {
