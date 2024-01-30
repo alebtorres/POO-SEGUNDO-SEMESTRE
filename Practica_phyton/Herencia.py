0d268164e971d24359bfe88f450531c5683ad10c
alebtorres <alexanderbordonez@gmail.com>
2024-01-30 16:37:01 -0500

    Herencia en python

---
+++ Practica_phyton/Herencia.py
@@ -0,0 +1,22 @@
+class Persona:
+    def __init__(self, nombre, edad, nacionalidad):
+        self.nombre = nombre
+        self.edad = edad
+        self.nacionalidad = nacionalidad
+    #primer metodo 
+    def hablar(self):
+        print("Esta hablando")
+        
+#pongo en parentesis la clase que voy a heredar        
+class Empleado(Persona):
+    def __init__(self, nombre, edad, nacionalidad, trabajo, sueldo):
+        super().__init__(nombre,edad,nacionalidad)
+        self.trabajo = trabajo
+        self.sueldo = sueldo
+    #sobreescribo el metodo de la clase padre para agregar algo
+    def hablar(self):
+        print ("No esta hablando")
+
+joel = Empleado("Joel", 21, "ecuatoriano", "Programador", 4500)
+# Herencia simple...
+joel.hablar()
